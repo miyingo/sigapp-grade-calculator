c67fd76f96bd759734931d7d62c7319bd2689358
jfranklin773 <jfranklin773@1c8373fd-5458-96cf-81fe-ca04b418d989>
2010-11-02 23:05:23 +0000

    updated database to include grades and components

--- trunk/Grade Calculator/src/application/grade/calculator/database/Database.java
+++ trunk/Grade Calculator/src/application/grade/calculator/database/Database.java
@@ -13,9 +13,9 @@ import application.grade.calculator.R;
 
 public class Database extends SQLiteOpenHelper {
 	private static final String DATABASE_NAME = "GradeCalc.db";
-	private static final int DATABASE_VERSION = 5;
+	private static final int DATABASE_VERSION = 8;
 	
-	//Exercise Database columns
+	//Classes Table Columns
 	public static final String CLASSES_TABLE = "classes";
 	public static final String _ID ="_id";	//int
 	public static final String NAME = "name";	//text
@@ -25,16 +25,44 @@ public class Database extends SQLiteOpenHelper {
 	public static final String YEAR = "year";	//int
 	public static final String PIC = "picture"; //int
 
+	//Components Table Columns
+	public static final String COMPONENTS_TABLE = "components";
+	//public static final String _ID ="_id";	//int
+	//public static final String NAME = "name";	//text
+	public static final String CLASS_ID = "class_id"; //int holds the class id that has this component
+	public static final String TOTAL_GRADE_MADE = "grade_made"; //double  holds grade made out of components from last update
+	public static final String TOTAL_GRADE_OUT_OF = "grade_out_of"; //double holds grade total out of components from last update
+	
+	//Grade Table Columns
+	public static final String GRADE_TABLE = "grades";
+	//public static final String _ID ="_id";	//int
+	//public static final String NAME = "name";	//text
+	public static final String COMPONENTS_ID = "class_id"; //int holds the class id that has this component
+	public static final String GRADE_MADE = "grade_made"; //double  holds grade made out of components from last update
+	public static final String GRADE_OUT_OF = "grade_out_of"; //double holds grade total out of components from last update
+
 
 	
-	public static final String DatabaseCreateString = "create table if not exists "+CLASSES_TABLE+" ("+_ID
+	public static final String DatabaseCreateClassesString = "create table if not exists "+CLASSES_TABLE+" ("+_ID
 						+" integer primary key autoincrement, "+NAME
 						+" text not null, "
 						+PIC+" text, "
 						+TOTAL_GRADE+" text, "
-						+YEAR+" integer, "+SEMESTER+" text);";
-
-	//Record
+						+YEAR+" float, "+SEMESTER+" text);";
+	
+	public static final String DatabaseCreateComponentsString = "create table if not exists "+COMPONENTS_TABLE+" ("+_ID
+						+" integer primary key autoincrement, "
+						+NAME+" text not null, "
+						+CLASS_ID+" float, "
+						+TOTAL_GRADE_MADE+" float, "
+						+TOTAL_GRADE_OUT_OF+" float);";
+	
+	public static final String DatabaseCreateGradesString = "create table if not exists "+COMPONENTS_TABLE+" ("+_ID
+						+" integer primary key autoincrement, "
+						+NAME+" text not null, "
+						+COMPONENTS_ID+" float, "
+						+GRADE_MADE+" float, "
+						+GRADE_OUT_OF+" float);";
 	
 	public Context ctx;
 	public SQLiteDatabase db;
@@ -50,12 +78,20 @@ public class Database extends SQLiteOpenHelper {
 	
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		db.execSQL(DatabaseCreateString);
+		db.execSQL(DatabaseCreateClassesString);
+		db.execSQL(DatabaseCreateComponentsString);
+		db.execSQL(DatabaseCreateGradesString);
 		addClass("CS180",db , 1);
 		addClass("PHYS172",db, 2);
 		addClass("PSY200",db, 1);
 		addClass("CS240",db , 3);
 		addClass("CS180",db , 1);
+		addComponents("HOMEWORK",db,77,89);
+		addComponents("Projects",db,73,83);
+		addGrade("Project1",db,73,82);
+		addGrade("Project2",db,73,83);
+
+
 	}
 
 	@Override
@@ -72,6 +108,28 @@ public class Database extends SQLiteOpenHelper {
 		values.put(SEMESTER, semester);
 		values.put(YEAR, 2010);
 		db.insert(CLASSES_TABLE, null, values);
+		
+	}
+	
+	public void addComponents(String name, SQLiteDatabase db, int grade_made, int grade_out){
+		
+		ContentValues values = new ContentValues();
+		values.put(NAME, name);
+		values.put(CLASS_ID, 1);
+		values.put(TOTAL_GRADE_MADE, grade_made);
+		values.put(TOTAL_GRADE_OUT_OF, grade_out );
+		db.insert(COMPONENTS_TABLE, null, values);
+		
+	}
+	
+	public void addGrade(String name, SQLiteDatabase db, int grade_made, int grade_out){
+		
+		ContentValues values = new ContentValues();
+		values.put(NAME, name);
+		values.put(COMPONENTS_ID, 1);
+		values.put(GRADE_MADE, grade_made);
+		values.put(GRADE_OUT_OF, grade_out );
+		db.insert(GRADE_TABLE, null, values);
 		
 	}
 	

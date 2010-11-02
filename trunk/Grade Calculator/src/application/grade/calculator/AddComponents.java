a390599df43f92710c86a4c5f7074801fced7f37
jfranklin773 <jfranklin773@1c8373fd-5458-96cf-81fe-ca04b418d989>
2010-11-02 22:26:49 +0000

    add ClassProfile.java file

--- trunk/Grade Calculator/src/application/grade/calculator/AddComponents.java
+++ trunk/Grade Calculator/src/application/grade/calculator/AddComponents.java
@@ -1,9 +1,9 @@
 package application.grade.calculator;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 
-public class AddComponents extends Activity {
+public class AddComponents extends ListActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
---
+++ trunk/Grade Calculator/src/application/grade/calculator/ClassProfile.java
@@ -0,0 +1,14 @@
+package application.grade.calculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ClassProfile extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.classprofile);
+	}
+
+}
